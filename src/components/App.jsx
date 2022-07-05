5e710f24913b9ab36acabe23bcfbfe8b01453976
RomaSamkov <Romansamkov.rs@gmail.com>
2022-07-05 22:13:55 +0300

    add Section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Section from './Section';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = state => {
+    this.setState(prevState => ({
+      [state]: prevState[state] + 1,
+    }));
+  };
+  render() {
+    // const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title="Please leave feedback"></Section>
+      </>
+    );
+  }
+}
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: auto;
+  max-width: 500px;
+  padding: 20px;
+  /* box-shadow: var(--card-shadow); */
+  text-align: center;
+`;
